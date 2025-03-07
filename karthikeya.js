f7a33533c9fbd85d93373e76f567d18f4f3412fa
harisubbarajusagi <159162271+harisubbarajusagi@users.noreply.github.com>
2025-03-07 18:13:27 +0530

    karthikeya.js

--- karthikeya.js
+++ karthikeya.js
@@ -4,10 +4,26 @@ function toggleMenu() {
 function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-}
- 
-let btn = document.getElementById('btn');
 
-btn.addEventListener("click",()=>{
-    window.scrollTo({top: 0, behavior: "smooth"});
-})
+   
+    if (element.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let btn = document.getElementById('btn');
+    btn.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
